b9c981042a5dc1f3058420b399edef897dceab64
Xinyuan Huang <xinyuahu@cisco.com>
2015-03-27 20:35:19 +0800

    add a setup file. (this is temporary!)

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# Copyright (c) 2014 Cisco Systems Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from distutils.core import setup
+
+setup(
+    name='openstack-nova-solver-scheduler',
+    version='0.1',
+    description='a constraint solver based scheduler for openstack compute',
+    author='Cisco Systems, Inc.',
+    url='https://github.com/CiscoSystems/nova-solver-scheduler',
+    packages=['nova', 'nova.scheduler', 'nova.scheduler.solvers',
+            'nova.scheduler.solvers.costs',
+            'nova.scheduler.solvers.constraints'],
+    package_dir={'nova': 'nova'},
+    #data_files=[('/etc/nova', ['etc/nova/solver_scheduler_conf.sample'])]
+    )
